abf75564c9d705593e6ade0a30d92a3b5a4043f0
minji7901 <88637782+minji7901@users.noreply.github.com>
2024-11-01 10:56:10 +0900

    refactor : separate files

---
+++ src/components/MedalFilter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { MdFilterAlt } from "react-icons/md";
+
+export default function MadalFilter({ selected, setSelected }) {
+  const filterList = ["gold", "silver", "bronze", "total"];
+
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
+
+  return (
+    <div className="flex gap-2 items-center justify-end mb-3">
+      <MdFilterAlt />
+      <select
+        name="medal-filter"
+        id="medalFilter"
+        onChange={handleSelect}
+        value={selected}
+      >
+        {filterList.map((item, i) => (
+          <option key={i} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
